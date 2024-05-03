c3e38ad203b7cc9e29f5f5f0f83b124d21bf1a90
Jared Lobbestael <jlobbes2@emich.edu>
2024-05-03 10:27:28 +0800

    REFACTOR: continuted refactor to allow for autoscroll presets

--- To-Do.md
+++ To-Do.md
@@ -1,13 +1,57 @@
-- Add dark mode
-- Remove text input box, change to button only
-- Reduce sliding blurr, increase frame rate? 
-- Add automatic scroll
+- ✔ Add dark mode
+- Remove text input box, change to button only 
+    - NOTE: permissions make juice not worth the squeeze.
+- Reduce sliding blurr, increase frame rate? (can't be done)
+- ✔ Add automatic scroll
+    - Add animation type options
+    - Add presets, not robust enought to allow fine user control
+        - Long swipes 
+            - this.scrollSpeed = 500; // pixels per interval
+            - this.frameRate = 1500; // milliseconds
+            - this.autoScrollAnimation = 'transform 1.5s ease-in-out';
+
+        - Med swipes
+            - this.scrollSpeed = 400; // pixels per interval
+            - this.frameRate = 1250; // milliseconds
+            - this.autoScrollAnimation = 'transform 1.250s ease-in-out';
+
+        - Short swipes
+            - this.scrollSpeed = 300; // pixels per interval
+            - this.frameRate = 1000; // milliseconds
+            - this.autoScrollAnimation = 'transform 1.0s ease-in-out';
+
+        - Turtle scroll
+            - this.scrollSpeed = 0.5; // pixels per interval
+            - this.frameRate = 10; // milliseconds
+            - this.autoScrollAnimation = '';
+
+        - Med scroll
+            - this.scrollSpeed = 1; // pixels per interval
+            - this.frameRate = 5; // milliseconds
+            - this.autoScrollAnimation = 'transfrom linear';
+
+        - Fast scroll
+            - this.scrollSpeed = 1.5; // pixels per interval
+            - this.frameRate = 5; // milliseconds
+            - this.autoScrollAnimation = 'transfrom linear';
+
+        - Fastest scroll
+            - this.scrollSpeed = 1.5; // pixels per interval
+            - this.frameRate = 5; // milliseconds
+            - this.autoScrollAnimation = 'transfrom linear';
+
 - Add adjustable display width
 - Add picture to text
 - Add broad navigation and/or tracking on page
 - Add instructions
 - Add setting saved option
-- Add full screen mode
+    - Add options to made slide more powerful
+    - Add button show options (customize display)
+    - Add options to swap scroll direction for keyboards
+- Add full screen mode for mobile
 - Add informational bubbles
 - Change buttons to collapsable panel
 - Change to extension
+- Add option for RSVP
+- Add word search
+- Add word/text tracker 
--- js/TextScroller.js
+++ js/TextScroller.js
@@ -1,18 +1,20 @@
 class TextScroller {
     constructor(initialText) {
-        this.text = initialText;
-        this.scrollerText = document.getElementById('text');
         this.scrollerBox = document.getElementsByClassName('text-wrapper')[0];
-        this.scrollerText.textContent = this.text; // Set the initial text
+        this.scrollerText = document.getElementById('text');
+        this.scrollerText.textContent = initialText; 
+
         this.translate = 0;
         this.isDown = false;
         this.cursorStartX = 0;
         this.offsetDuringDrag = 0;
-        // new code ***********
+
         this.autoScrollInterval = null;
         this.scrollSpeed = 2; // pixels per interval
-        this.autoScrollPaused = false; // use when user scroll with autoScroll on
-        // new code end ************
+        this.frameRate = 5; // milliseconds
+        this.autoScrollAnimation = 'transfrom linear';
+        this.autoScrollPaused = false; // used to pause autoscroll on user swipe action
+
 
         this.addEventListeners();
         this.applySmoothAnimation();
@@ -72,11 +74,15 @@ class TextScroller {
         this.scrollerText.style.transition = '';
     }
 
+    applyAutoScrollAnimation() {
+        this.scrollerText.style.transition = this.autoScrollAnimation;
+    }
+
     keyboardControl(e) {
         const step = 60;
-        if (e.key === "ArrowLeft") {
+        if (e.key === "ArrowRight") {
             this.translate -= step;
-        } else if (e.key === "ArrowRight") {
+        } else if (e.key === "ArrowLeft") {
             this.translate += step;
         }
         this.updatePosition(this.translate);
@@ -91,7 +97,7 @@ class TextScroller {
     setText() {
         const userTextBox = document.getElementById('textInput');
         const userTextInput = userTextBox.value;
-        userTextBox.value = ''; // Clear the user-input textarea]
+        userTextBox.value = ''; // Clear the user-input textarea
 
         this.scrollerText.textContent = userTextInput;
         this.centerText();
@@ -107,9 +113,11 @@ class TextScroller {
 
     startAutoScroll() {
         this.autoScrollInterval = setInterval(() => {
-            this.translate -= this.scrollSpeed; // Modify this to adjust the scroll direction and speed
+            this.translate -= this.scrollSpeed; 
+            this.removeSmoothAnimation();
+            this.applyAutoScrollAnimation();
             this.updatePosition(this.translate);
-        }, 10); // Update every 20 milliseconds
+        }, this.frameRate); // frameRate is not FPS, but ms between animation
         this.toggleAutoScrollIcons('stopIcon');
     }
 
@@ -137,7 +145,33 @@ class TextScroller {
 
 // Usage
 document.addEventListener('DOMContentLoaded', () => {
-    const currentScroller = new TextScroller('This is a block of initial text.');
+    const currentScroller = new TextScroller(
+        `
+        In the heart of Ethiopia lies an extraordinary geological marvel—the 
+        Danakil Depression, one of the hottest and most alien landscapes on Earth. 
+        This cauldron-like depression is part of the Afar Triangle, a junction of 
+        three tectonic plates, which are pulling apart from each other, creating 
+        new land as magma surfaces. Here, temperatures can soar above 120°F, and 
+        the air shimmers above vast salt flats and hydrothermal fields.
+
+        The Danakil Depression offers a glimpse into Earth’s inner workings, 
+        featuring dazzling formations like Dallol. Dallol holds the record for the
+         highest average temperatures for an inhabited location on Earth and 
+         showcases bright yellow sulfur fields, green acid pools, and iron-rich 
+         red waters. These psychedelic landscapes are not just a draw for intrepid 
+         tourists; they are invaluable to scientists studying extremophile microbes 
+         thriving in such harsh conditions. These studies provide clues about life's 
+         adaptability and even implications for extraterrestrial life, suggesting that 
+         life could potentially exist in similar environments on other planets.
+
+         Amidst this severe environment, the Afar people have adapted to extract 
+         precious salt, continuing a centuries-old tradition. Caravans of camels 
+         and donkeys, led by the Afar, trek across the desert, harvesting salt from 
+         the ground, cut into slabs, and transported to markets across Ethiopia. 
+         This blend of extreme natural phenomena and human endurance paints a vivid 
+         picture of life's tenacity and the dynamic forces shaping our planet.    
+        `
+    );
     currentScroller.centerText();
     
     
