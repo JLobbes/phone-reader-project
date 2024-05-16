f1691bfcabc92a2e72a617bf418e56f567efacca
Jared Lobbestael <jlobbes2@emich.edu>
2024-05-16 10:27:49 +0800

    FIX: fixed user text input display issue (not resetting)

--- js/toolbar.js
+++ js/toolbar.js
@@ -3,44 +3,60 @@ const toolbar = new ButtonPanel('toolbar');
 toolbar.pasteZoneStatus = 'open';
 toolbar.pasteButton = toolbar.panel.querySelector('#pasteText');
 toolbar.pasteTextZone = toolbar.panel.querySelector('#textInput');
+toolbar.textInputGoButton = toolbar.panel.querySelector('#goButton');
 toolbar.textInputButtonContainer = toolbar.pasteButton.parentElement;
 
 toolbar.openPasteZone = function() {
     if(this.pasteZoneStatus === 'closed') {
+        // Make pasteZone visible
         this.addVisibleClass(this.pasteTextZone);
+
+        // Add inline styles to override default styles
         this.textInputButtonContainer.style.display = 'flex';
         this.textInputButtonContainer.style.justifyContent = 'center';
         this.textInputButtonContainer.style.alignContent = 'center';
-        this.textInputButtonContainer.style.width = '125px';
+        this.textInputButtonContainer.style.width = '130px';
+        this.textInputGoButton.style.width = '40px';
+
         this.pasteZoneStatus = 'open';
     }
     
     if(this.pasteTextZone.classList.contains('visible')) { 
         setTimeout(() => {
             this.pasteTextZone.focus();
-            // To-Do: Incorporate longpress mimic for mobile
-            //        Logic start is below
-            //        Will requires mobile state check
         }, 50);
     }
 }
 
 toolbar.closePasteZone = function() {
     if(this.pasteZoneStatus === 'open') {
+        // Remove visibility
         this.removeVisibleClass(this.pasteTextZone);
+
+        // Remove inline styles so default animation and styling can occur
         this.textInputButtonContainer.style.display = '';
         this.textInputButtonContainer.style.width = '';
+        this.textInputGoButton.style.width = '';
+
+        // Ensure textInput module gets reset to proper status
+        setTimeout(() => {
+            this.textInputGoButton.classList.remove('current');
+            this.addHiddenClass(this.textInputGoButton);
+
+            this.pasteButton.classList.add('current');
+            this.removeHiddenClass(this.pasteButton);
+        }, 100);
         this.pasteZoneStatus = 'closed';
     }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     toolbar.closePasteZone();
-
-    const pasteButton = toolbar.panel.querySelector('#pasteText');
-    pasteButton.addEventListener('click', () => toolbar.openPasteZone());
-    
+    toolbar.pasteButton.addEventListener('click', () => toolbar.openPasteZone());
+    toolbar.closePanelButton.addEventListener('click', () => toolbar.closePasteZone());
 });
+
+modules.export = toolbar;
 
 // To-Do incorportate longpress mimic for mobile!!
 // const element = document.getElementById('myElement');
